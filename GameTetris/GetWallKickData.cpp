df15b2f0aa25a5f23937c2ca79f6c95e3c5b531c
Pimkhwan <kanyawe34854@gmail.com>
2022-02-10 23:40:37 +0700

    add case

--- GameTetris/GetWallKickData.cpp
+++ GameTetris/GetWallKickData.cpp
@@ -1,86 +1,107 @@
 #include <vector>
 
 #include "Global.h"
-#include "GetTetromino.h"
+#include "GetWallKickData.h"
 
-std::vector<Position> get_tetromino(unsigned char i_shape, unsigned char i_x, unsigned char i_y)
+std::vector<Position> get_wall_kick_data(bool i_is_i_shape, unsigned char i_current_rotation, unsigned char i_next_rotation)
 {
-	//I'm pretty sure the code is self-explanatory
-	std::vector<Position> output_tetromino(4);
+	//We return the wall kick data based on the current and next rotations. We also check whether the shape is I or not
+	//Fun fact, the website that provided the data had the y-coordinate flipped so I had to flip every y's sign
+	//I have NO idea why did they even do this
 
-	//We just set the relative position for every mino
-	switch (i_shape)
+	if (0 == i_is_i_shape)
 	{
-	case 0:
-	{
-		output_tetromino[0] = { 1, -1 };
-		output_tetromino[1] = { 0, -1 };
-		output_tetromino[2] = { -1, -1 };
-		output_tetromino[3] = { -2, -1 };
+		switch (i_current_rotation)
+		{
+		case 0:
+		case 2:
+		{
+			switch (i_next_rotation)
+			{
+			case 1:
+			{
+				return { {0, 0}, {-1, 0}, {-1, -1}, {0, 2}, {-1, 2} };
+			}
+			case 3:
+			{
+				return { {0, 0}, {1, 0}, {1, -1}, {0, 2}, {1, 2} };
+			}
+			}
+		}
+		case 1:
+		{
+			return { {0, 0}, {1, 0}, {1, 1}, {0, -2}, {1, -2} };
+		}
+		case 3:
+		{
+			return { {0, 0}, {-1, 0}, {-1, 1}, {0, -2}, {-1, -2} };
+		}
+		}
 
-		break;
+		return { {0, 0} };
 	}
-	case 1:
+	else
 	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 1, 0 };
-		output_tetromino[2] = { -1, -1 };
-		output_tetromino[3] = { -1, 0 };
+		switch (i_current_rotation)
+		{
+		case 0:
+		{
+			switch (i_next_rotation)
+			{
+			case 1:
+			{
+				return { {0, 0}, {-2, 0}, {1, 0}, {-2, 1}, {1, -2} };
+			}
+			case 3:
+			{
+				return { {0, 0}, {-1, 0}, {2, 0}, {-1, -2}, {2, 1} };
+			}
+			}
+		}
+		case 1:
+		{
+			switch (i_next_rotation)
+			{
+			case 0:
+			{
+				return { {0, 0}, {2, 0}, {-1, 0}, {2, -1}, {-1, 2} };
+			}
+			case 2:
+			{
+				return { {0, 0}, {-1, 0}, {2, 0}, {-1, -2}, {2, 1} };
+			}
+			}
+		}
+		case 2:
+		{
+			switch (i_next_rotation)
+			{
+			case 1:
+			{
+				return { {0, 0}, {1, 0}, {-2, 0}, {1, 2}, {-2, -1} };
+			}
+			case 3:
+			{
+				return { {0, 0}, {2, 0}, {-1, 0}, {2, -1}, {-1, 2} };
+			}
+			}
+		}
+		case 3:
+		{
+			switch (i_next_rotation)
+			{
+			case 0:
+			{
+				return { {0, 0}, {1, 0}, {-2, 0}, {1, 2}, {-2, -1} };
+			}
+			case 2:
+			{
+				return { {0, 0}, {-2, 0}, {1, 0}, {-2, 1}, {1, -2} };
+			}
+			}
+		}
+		}
 
-		break;
+		return { {0, 0} };
 	}
-	case 2:
-	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 1, 0 };
-		output_tetromino[2] = { 1, -1 };
-		output_tetromino[3] = { -1, 0 };
-
-		break;
-	}
-	case 3:
-	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 0, -1 };
-		output_tetromino[2] = { -1, -1 };
-		output_tetromino[3] = { -1, 0 };
-
-		break;
-	}
-	case 4:
-	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 1, -1 };
-		output_tetromino[2] = { 0, -1 };
-		output_tetromino[3] = { -1, 0 };
-
-		break;
-	}
-	case 5:
-	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 1, 0 };
-		output_tetromino[2] = { 0, -1 };
-		output_tetromino[3] = { -1, 0 };
-
-		break;
-	}
-	case 6:
-	{
-		output_tetromino[0] = { 0, 0 };
-		output_tetromino[1] = { 1, 0 };
-		output_tetromino[2] = { 0, -1 };
-		output_tetromino[3] = { -1, -1 };
-	}
-	}
-
-	//Then we add the given x and y to the minos
-	for (Position& mino : output_tetromino)
-	{
-		mino.x += i_x;
-		mino.y += i_y;
-	}
-
-	//Return the tetromino
-	return output_tetromino;
 }
