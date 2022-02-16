385f23b85989b2e25595dfbb004bc0e2bcd8e3d0
Pimkhwan <kanyawe34854@gmail.com>
2022-02-16 23:08:23 +0700

    add Menu

---
+++ GameTetris/Menu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#define MAX_ITEM 3 //start leaderbaord exit
+class Menu
+{
+	public:
+		Menu(float width, float height);
+		~Menu();
+
+		void drawMenu(sf::RenderWindow& window);
+		void MoveUp();
+		void Movedown();
+		int GetPressedItem() { return selectedItem; }
+	private:
+		int selectedItem;
+		sf::Font font;
+		sf::Text menu[MAX_ITEM];
+};
+
